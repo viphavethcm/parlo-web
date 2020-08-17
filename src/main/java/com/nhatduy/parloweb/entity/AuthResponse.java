6a7c954d585fe5674ec6f32bf5811e1434fa7157
DuyDev <loivang.nhatduy@gmail.com>
2020-08-17 11:33:01 +0700

    API login

--- src/main/java/com/nhatduy/parloweb/entity/AuthResponse.java
+++ src/main/java/com/nhatduy/parloweb/entity/AuthResponse.java
@@ -1,10 +1,16 @@
 package com.nhatduy.parloweb.entity;
 
 public class AuthResponse {
+    private final String userName;
     private final String token;
 
-    public AuthResponse( String token) {
+    public AuthResponse(String userName, String token) {
+        this.userName = userName;
         this.token = token;
+    }
+
+    public String getUserName() {
+        return userName;
     }
 
     public String getToken() {
--- src/main/java/com/nhatduy/parloweb/utils/HeaderUtils.java
+++ src/main/java/com/nhatduy/parloweb/utils/HeaderUtils.java
@@ -1,9 +1,11 @@
 package com.nhatduy.parloweb.utils;
 
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.http.HttpHeaders;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class HeaderUtils {
 
@@ -15,20 +17,19 @@ public class HeaderUtils {
         }
         return headerUtils;
     }
-//jádasdádasd
 
-    public Cookie setCookie(HttpServletResponse response,String userName){
-        Cookie cookie = new Cookie("userName",userName);
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(24*60*60*60);
-        response.addCookie(cookie);
-        return cookie;
-    }
+//    public Cookie setCookie(String sessionID){
+//        Cookie cookie = new Cookie("JSESSIONID",sessionID);
+//        cookie.setPath("/");
+//        cookie.setMaxAge(24*60*60*10);
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
+//        return cookie;
+//    }
 
-    public HttpHeaders setHeaders(Cookie cookie){
+    public HttpHeaders setHeaders(){
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type","application/json");
-        headers.add("Cookie",cookie.getValue());
         return headers;
     }
 
