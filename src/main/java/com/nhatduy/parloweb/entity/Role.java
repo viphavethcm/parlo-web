910f9c2181a9717b01d09d7c08325e4785c033cd
viphavethcm <loivang.nhatduy@gmail.com>
2020-11-14 11:17:00 +0700

    update

--- src/main/java/com/nhatduy/parloweb/entity/Role.java
+++ src/main/java/com/nhatduy/parloweb/entity/Role.java
@@ -11,7 +11,7 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "RoleID")
+    @Column(name = "role_Id")
     private int roleID;
 
     @Column(name = "RoleName")
